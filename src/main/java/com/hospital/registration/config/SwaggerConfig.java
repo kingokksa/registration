a22b2cb5473445b3e1964233b98ab46b1e90c87c
kingokksa <kingokksa@qq.com>
2025-05-10 21:26:07 +0800

    swagger安装

---
+++ src/main/java/com/hospital/registration/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.hospital.registration.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("医院挂号系统API")
+                        .version("1.0")
+                        .description("API 文档"));
+    }
+}
